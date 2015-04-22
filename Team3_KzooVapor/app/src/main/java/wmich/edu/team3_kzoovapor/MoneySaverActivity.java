e21f65bc23b3411db05761653fe5197c418d8634
ansonr <ansoniam2000@gmail.com>
2015-04-22 00:27:40 -0400

    More work done on Money Saver
    Its further along but still not done, working out a couple bugs

--- Team3_KzooVapor/app/src/main/java/wmich/edu/team3_kzoovapor/MoneySaverActivity.java
+++ Team3_KzooVapor/app/src/main/java/wmich/edu/team3_kzoovapor/MoneySaverActivity.java
@@ -2,16 +2,81 @@ package wmich.edu.team3_kzoovapor;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.text.NumberFormat;
 
 
 public class MoneySaverActivity extends ActionBarActivity {
+
+    private static final NumberFormat currencyFormater = NumberFormat.getCurrencyInstance();
+    private double packsPerW, costPerP, eJuicePerW, costPerM;
+    private EditText packsPerWeekEdit, costPerPackEdit, eJuicePerWeekEdit, costPerMiliEdit;
+    private TextView savingsPerWeek;
+
+    private double amountSaved;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_money_saver);
+
+        //instantiate the edit texts
+        packsPerWeekEdit = (EditText) findViewById(R.id.editTextPacksPerWeek);
+        costPerPackEdit = (EditText) findViewById(R.id.editTextCostPerPack);
+        eJuicePerWeekEdit = (EditText) findViewById(R.id.editTextJuicePerWeek);
+        costPerMiliEdit = (EditText) findViewById(R.id.editTextCostPerMillileter);
+
+        //instantiate the text view
+        savingsPerWeek = (TextView) findViewById(R.id.textViewSavedTotal);
+
+        packsPerWeekEdit.addTextChangedListener(editTextWatcher);
+
+
+
+
+
+
+    }
+
+
+    private TextWatcher editTextWatcher = new TextWatcher() {
+
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+
+            packsPerW = Double.parseDouble(s.toString());
+            costPerP = Double.parseDouble(s.toString());
+            eJuicePerW = Double.parseDouble(s.toString());
+            costPerM = Double.parseDouble(s.toString());
+
+            savingsPerWeek.setText(currencyFormater.format(amountSaved));
+                    calcTotalSaved();
+
+        }
+        //starts tracking when the seek bar is touched
+        public void afterTextChanged(Editable s)
+        {
+        }
+
+        //stops tracking when the seek bar is no longer touched
+        public void beforeTextChanged(CharSequence s, int start, int count,int after)
+        {
+        }
+    };
+
+    private void calcTotalSaved(){
+
+        double cigTotal = (packsPerW * costPerP);
+        double juiceTotal = (eJuicePerW * costPerM);
+
+        amountSaved = ((juiceTotal - cigTotal) * -1);
+
     }
 
 
